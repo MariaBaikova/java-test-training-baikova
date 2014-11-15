9ef403c4615cc41b964ebb659c0f7c8b5bebad0c
Baikova <marybaikova24@gmail.com>
2014-11-16 02:29:33 +0300

    HomeWork_13

---
+++ addressbook-selenium-tests/runTest.bat
@@ -0,0 +1,4 @@
+cd C:\Temp\Selenium_and_java\addressbook-selenium-tests
+java -cp bin;C:\Temp\Selenium_and_java\xstream\xstream-1.4.7\lib\xstream-1.4.7.jar;C:\Temp\Selenium_and_java\xstream\xstream-1.4.7\lib\xstream\xpp3_min-1.1.4c.jar;C:\Temp\Selenium_and_java\xstream\xstream-1.4.7\lib\xstream\xmlpull-1.1.3.1.jar;C:\Temp\Selenium_and_java\selenium-server-standalone-2.43.1.jar org.testng.TestNG testng-customsuite.xml
+pause 100
+
