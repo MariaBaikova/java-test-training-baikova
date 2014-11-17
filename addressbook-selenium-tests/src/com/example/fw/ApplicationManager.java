49f6218c81622d8f06853c194d9375356dddd705
MariaBaikova <mashynka2410@mail.ru>
2014-11-17 19:20:57 +0300

    HomeWork_14

--- addressbook-selenium-tests/src/com/example/fw/ApplicationManager.java
+++ addressbook-selenium-tests/src/com/example/fw/ApplicationManager.java
@@ -1,24 +1,40 @@
 package com.example.fw;
 
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import com.opera.core.systems.OperaDriver;
 
 
 public class ApplicationManager {
 	public WebDriver driver;
 	public String baseUrl;
-	
+	private Properties properties;
 	private NavigationHelper navigationHelper;
 	private GroupHelper groupHelper;
 	private ContactHelper contactHelper;
 	
-	public ApplicationManager(){
-		driver = new FirefoxDriver();
-	    baseUrl = "http://localhost/";
-	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	    driver.get(baseUrl + "/addressbookv4.1.4/");
+	public ApplicationManager(Properties properties){
+		this.properties = properties;
+		String browser = properties.getProperty("browser");
+		baseUrl = properties.getProperty("baseUrl");
+		if ("firefox".equals(browser)){
+			driver = new FirefoxDriver();
+		}else if ("opera".equals(browser)){
+			driver = new OperaDriver();
+		}else if ("ie".equals(browser)){
+			driver = new InternetExplorerDriver();
+		}else{
+			throw new Error("Unsupported browser: " + browser);
+		}
+		
+	   // baseUrl = "http://localhost/";
+	    driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+	    driver.get(baseUrl);
 	}
 	
 	public void stop() {
--- addressbook-selenium-tests/src/com/example/tests/TestBase.java
+++ addressbook-selenium-tests/src/com/example/tests/TestBase.java
@@ -1,8 +1,11 @@
 package com.example.tests;
 
+import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Properties;
 
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -10,13 +13,17 @@ import org.testng.annotations.DataProvider;
 
 import static com.example.tests.GroupDataGenerator.generateRandomGroups;
 import static com.example.tests.ContactDataGenerator.generateRandomContacts;
+
 import com.example.fw.ApplicationManager;
 public class TestBase {
 	protected ApplicationManager app;
 	
 	@BeforeTest 
 	public void setUp() throws Exception {
-	    app = new ApplicationManager();
+		String configFile = System.getProperty("configFile","application.properties");
+		Properties properties = new Properties();
+		properties.load(new FileReader(new File(configFile)));
+	    app = new ApplicationManager(properties);
 	  }
 	
 	@AfterTest 
